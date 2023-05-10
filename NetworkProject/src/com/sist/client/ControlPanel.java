477edfc0d18f25c65e9061566b0f0adadeeba54e
SIST <SIST@DESKTOP-2VEJONH>
2023-05-11 08:55:47 +0900

    1111

--- NetworkProject/src/com/sist/client/ControlPanel.java
+++ NetworkProject/src/com/sist/client/ControlPanel.java
@@ -2,21 +2,33 @@ package com.sist.client;
 import java.awt.CardLayout;
 import javax.swing.JPanel;
 public class ControlPanel extends JPanel {
-	HomePanel hp = new HomePanel();
-	ChartPanel cp = new ChartPanel();
-	FindPanel fp = new FindPanel();
+	HomePanel hp; 
+	ChartPanel cp;
+	DetailPanel dp;
+	FindPanel fp = new FindPanel(); //화면 전환 불가능
 	ChatPanel chatp = new ChatPanel();
-	MagPanel mp = new MagPanel();
-	
+	BoardListPanel bp;
+	BoardInsertPanel ip;
+	BoardDetailPanel bdp;
 	CardLayout card = new CardLayout();
 	
 	public ControlPanel()
 	{
+		bp=new BoardListPanel(this);//화면 전환 가능
+		ip=new BoardInsertPanel(this);
+    	bdp=new BoardDetailPanel(this);
+		cp = new ChartPanel(this);
+		dp = new DetailPanel(this);
+		hp = new HomePanel(this);
 		setLayout(card);
 		add("home",hp);
+		add("detail",dp);
+		add("board",bp);
+    	add("insert",ip);
 		add("chart",cp);
 		add("find",fp);
 		add("chat",chatp);
-		add("magazine", mp);
+		add("bdp",bdp);
+		
 	}
 }
